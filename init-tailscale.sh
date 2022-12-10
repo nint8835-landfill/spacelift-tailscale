c34b9a8c5b99ac684c242be8cc6897640925db76
Riley Flynn <riley@rileyflynn.me>
2022-12-10 15:07:03 -0330

    Remove loop
    I very much hope this doesn't trigger another infinite init

--- init-tailscale.sh
+++ init-tailscale.sh
@@ -3,10 +3,7 @@
 tailscaled --tun=userspace-networking --outbound-http-proxy-listen=localhost:1054 --socks5-server=localhost:1055 --socket=/tmp/tailscaled.sock 2>~/tailscaled.log &
 
 HOSTNAME="spacelift-$(cat /etc/hostname)"
-until tailscale --socket=/tmp/tailscaled.sock up --authkey ${TAILSCALE_AUTH_KEY} --hostname=${HOSTNAME} --accept-routes
-do
-    sleep 1
-done
+tailscale --socket=/tmp/tailscaled.sock up --authkey ${TAILSCALE_AUTH_KEY} --hostname=${HOSTNAME} --accept-routes
 
 export ALL_PROXY=socks5h://localhost:1055/
 export HTTP_PROXY=http://localhost:1054/
